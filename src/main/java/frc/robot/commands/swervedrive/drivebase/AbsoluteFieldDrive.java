330fc4e94aacfe23d0ce6c4d30974a44498b1d6c
Max <nguyenmax2008@gmail.com>
2024-01-12 23:18:57 -0500

    fixed file

--- src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
@@ -19,29 +19,34 @@ import swervelib.math.SwerveMath;
 /**
  * An example command that uses an example subsystem.
  */
-public class AbsoluteFieldDrive extends Command
-{
+public class AbsoluteFieldDrive extends Command {
 
   private final SwerveSubsystem swerve;
-  private final DoubleSupplier  vX, vY, heading;
+  private final DoubleSupplier vX, vY, heading;
 
   /**
-   * Used to drive a swerve robot in full field-centric mode.  vX and vY supply translation inputs, where x is
-   * torwards/away from alliance wall and y is left/right. headingHorzontal and headingVertical are the Cartesian
-   * coordinates from which the robot's angle will be derived— they will be converted to a polar angle, which the robot
+   * Used to drive a swerve robot in full field-centric mode. vX and vY supply
+   * translation inputs, where x is
+   * torwards/away from alliance wall and y is left/right. headingHorzontal and
+   * headingVertical are the Cartesian
+   * coordinates from which the robot's angle will be derived— they will be
+   * converted to a polar angle, which the robot
    * will rotate to.
    *
    * @param swerve  The swerve drivebase subsystem.
-   * @param vX      DoubleSupplier that supplies the x-translation joystick input.  Should be in the range -1 to 1 with
-   *                deadband already accounted for.  Positive X is away from the alliance wall.
-   * @param vY      DoubleSupplier that supplies the y-translation joystick input.  Should be in the range -1 to 1 with
-   *                deadband already accounted for.  Positive Y is towards the left wall when looking through the driver
+   * @param vX      DoubleSupplier that supplies the x-translation joystick input.
+   *                Should be in the range -1 to 1 with
+   *                deadband already accounted for. Positive X is away from the
+   *                alliance wall.
+   * @param vY      DoubleSupplier that supplies the y-translation joystick input.
+   *                Should be in the range -1 to 1 with
+   *                deadband already accounted for. Positive Y is towards the left
+   *                wall when looking through the driver
    *                station glass.
    * @param heading DoubleSupplier that supplies the robot's heading angle.
    */
   public AbsoluteFieldDrive(SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY,
-                            DoubleSupplier heading)
-  {
+      DoubleSupplier heading) {
     this.swerve = swerve;
     this.vX = vX;
     this.vY = vY;
@@ -51,25 +56,23 @@ public class AbsoluteFieldDrive extends Command
   }
 
   @Override
-  public void initialize()
-  {
+  public void initialize() {
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute()
-  {
+  public void execute() {
 
     // Get the desired chassis speeds based on a 2 joystick module.
 
     ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(),
-                                                         new Rotation2d(heading.getAsDouble() * Math.PI));
+        new Rotation2d(heading.getAsDouble() * Math.PI));
 
     // Limit velocity to prevent tippy
     Translation2d translation = SwerveController.getTranslation2d(desiredSpeeds);
     translation = SwerveMath.limitVelocity(translation, swerve.getFieldVelocity(), swerve.getPose(),
-                                           Constants.LOOP_TIME, Constants.ROBOT_MASS, List.of(Constants.CHASSIS),
-                                           swerve.getSwerveDriveConfiguration());
+        Constants.LOOP_TIME, Constants.ROBOT_MASS, List.of(Constants.CHASSIS),
+        swerve.getSwerveDriveConfiguration());
     SmartDashboard.putNumber("LimitedTranslation", translation.getX());
     SmartDashboard.putString("Translation", translation.toString());
 
@@ -80,16 +83,13 @@ public class AbsoluteFieldDrive extends Command
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted)
-  {
+  public void end(boolean interrupted) {
   }
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished()
-  {
+  public boolean isFinished() {
     return false;
   }
-
 
 }
